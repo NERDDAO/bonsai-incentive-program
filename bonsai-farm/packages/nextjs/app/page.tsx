ecef998f6c34e22b9151ba4bb02f5bb378c7c862
raigal-r <raquel.raigal@outlook.es>
2024-03-27 13:50:14 -0300

    welcome page

--- bonsai-farm/packages/nextjs/app/page.tsx
+++ bonsai-farm/packages/nextjs/app/page.tsx
@@ -1,66 +1,53 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+/* eslint-disable @next/next/no-img-element */
 "use client";
 
+import { useEffect, useState } from "react";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 import type { NextPage } from "next";
 import { useAccount } from "wagmi";
-import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { Address } from "~~/components/scaffold-eth";
 
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
 const Home: NextPage = () => {
+  //const router = useRouter();
+  const [isConnected, setIsConnected] = useState(false);
   const { address: connectedAddress } = useAccount();
+
+  useEffect(() => {
+    // Check if connectedAddress exists and is not null
+    if (connectedAddress) {
+      setIsConnected(true);
+      //router.push("/debug");
+      // Push to the debug page when connectedAddress changes
+    }
+  }, [connectedAddress]);
 
   return (
     <>
-      <div className="flex items-center flex-col flex-grow pt-10">
-        <div className="px-5">
-          <h1 className="text-center">
-            <span className="block text-2xl mb-2">Welcome to</span>
-            <span className="block text-4xl font-bold">Scaffold-ETH 2</span>
-          </h1>
-          <div className="flex justify-center items-center space-x-2">
-            <p className="my-2 font-medium">Connected Address:</p>
-            <Address address={connectedAddress} />
+      <div className="backdrop-blur-md">
+        <div className="flex items-center flex-col pt-20 bg-[url('/bonsai-landscape-2.png')] bg-cover bg-center">
+          <div className="px-5">
+            <h1 className="text-center">
+              <span className="block text-8xl mb-2">盆栽農園</span>
+              <span className="block text-4xl font-bold">へようこそ</span>
+            </h1>
+            <p className="text-center text-lg">Mint your Lucky Nest and start earning $Bonsai</p>
           </div>
-          <p className="text-center text-lg">
-            Get started by editing{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/nextjs/app/page.tsx
-            </code>
-          </p>
-          <p className="text-center text-lg">
-            Edit your smart contract{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              YourContract.sol
-            </code>{" "}
-            in{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/hardhat/contracts
-            </code>
-          </p>
-        </div>
-
-        <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
-          <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <BugAntIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Tinker with your smart contract using the{" "}
-                <Link href="/debug" passHref className="link">
-                  Debug Contracts
-                </Link>{" "}
-                tab.
-              </p>
-            </div>
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <MagnifyingGlassIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Explore your local transactions with the{" "}
-                <Link href="/blockexplorer" passHref className="link">
-                  Block Explorer
-                </Link>{" "}
-                tab.
-              </p>
-            </div>
+          <div>
+            <img src="logo-bonsai-farm.png" alt="Description of the image" className="w-full h-auto" />
+          </div>
+          <div className="text-center text-lg mb-5">
+            <ConnectButton />
           </div>
         </div>
       </div>
